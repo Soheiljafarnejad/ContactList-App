3725e1ca5fabb98f5631280b629474ca98be9f12
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-03-28 22:24:20 +0430

    Form submitHandler

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import { useState } from "react";
 import FormContact from "./Components/FormContact/FormContact";
 import HeaderCom from "./Components/Header/HeaderCom";
 import "./App.css";
 import ContactList from "./Components/ContactList/ContactList";
 const App = () => {
+  const [contact, setContact] = useState([]);
+  console.log(contact);
+
   return (
     <>
       <HeaderCom />
       <main className="main">
-        <FormContact />
+        <FormContact contact={contact} setContact={setContact} />
         <ContactList />
       </main>
     </>
--- src/Components/FormContact/FormContact.js
+++ src/Components/FormContact/FormContact.js
@@ -1,13 +1,44 @@
+import { useState } from "react";
 import style from "./FormContact.module.css";
-const FormContact = () => {
+const FormContact = ({ contact, setContact }) => {
+  const [value, setValue] = useState({ name: "", email: "" });
+
+  const changeHandler = (e) => {
+    setValue({ ...value, [e.target.id]: e.target.value });
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (!value.name || !value.email) {
+      alert("please enter value");
+      return;
+    }
+    setContact([...contact, value]);
+    setValue({ name: "", email: "" });
+  };
   return (
     <section className="container">
       <h3>Add Contact</h3>
-      <form className={style.form}>
+
+      <form className={style.form} onSubmit={submitHandler}>
         <label htmlFor="name">Name</label>
-        <input type="text" id="name" placeholder="Name" />
+        <input
+          type="text"
+          id="name"
+          placeholder="Name"
+          onChange={changeHandler}
+          value={value.name}
+        />
+
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" placeholder="Email" />
+        <input
+          type="email"
+          id="email"
+          placeholder="Email"
+          onChange={changeHandler}
+          value={value.email}
+        />
+
         <button>Add</button>
       </form>
     </section>
